9d729e79189708a93b7f10b40a86948a000448fe
Harpreet Singh <harpreet@gomadinfotech.com>
2018-12-14 14:11:35 +0530

    Add project files.

---
+++ Xero.Api.Example.MVC/Controllers/OrganisationController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using Xero.Api.Example.Applications.Public;
+using Xero.Api.Example.MVC.Helpers;
+
+namespace Xero.Api.Example.MVC.Controllers
+{
+    public class OrganisationController : Controller
+    {
+        public ActionResult Index()
+        {
+            var api = XeroApiHelper.CoreApi();
+
+            //var api1 = XeroApiHelper.User();
+
+            try
+            {
+                var organisation = api.Organisation;
+
+                
+                //var updated_emp = api.Update(new Employee
+                //{
+                //    Id = emp.Id,
+                //    Status = Xero.Api.Payroll.Australia.Model.Status.EmploymentStatus.Terminated,
+                //    TerminationDate = DateTime.Now.AddDays(-1)
+
+                //});
+
+
+
+                return View(organisation);
+            }
+            catch (RenewTokenException e)
+            {
+                return RedirectToAction("Connect", "Home");
+            }   
+        }
+    }
+}
---
+++ Xero.Api.Example.MVC/Helpers/XeroApiHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Xero.Api.Core;
+using Xero.Api.Example.Applications.Partner;
+using Xero.Api.Example.Applications.Public;
+using Xero.Api.Example.MVC.Stores;
+using Xero.Api.Infrastructure.Interfaces;
+using Xero.Api.Infrastructure.OAuth;
+using Xero.Api.Serialization;
+
+namespace Xero.Api.Example.MVC.Helpers
+{
+    public class ApplicationSettings
+    {
+        public string BaseApiUrl { get; set; }
+        public Consumer Consumer { get; set; }
+        public object Authenticator { get; set; }
+    }
+
+    public static class XeroApiHelper
+    {
+        private static ApplicationSettings _applicationSettings;
+
+        static XeroApiHelper()
+        {
+            // Refer to README.md for details
+            ////var callbackUrl = "http://mywebsite.url/Home/Authorize";
+
+            var callbackUrl = "http://localhost:61795/Home/Authorize";
+
+        
+            var memoryStore = new MemoryAccessTokenStore();
+            var requestTokenStore = new MemoryRequestTokenStore();
+            var baseApiUrl = "https://api.xero.com";
+
+            // Consumer details for Application
+            var consumerKey = "OQ82O70ISMCPNPDTGI7BHYEIZE6INU";
+            var consumerSecret = "R9N6MREI3G3ILBMUKKHKWNL4QJH6QY";
+
+            // Signing certificate details for Partner Applications
+            ////var signingCertificatePath = @"C:\Dev\your_public_privatekey.pfx";
+            ////var signingCertificatePassword = "Your_signing_cert_password - leave empty if you didn't set one when creating the cert";
+
+            // Public Application Settings
+            var publicConsumer = new Consumer(consumerKey, consumerSecret);
+
+            var publicAuthenticator = new PublicMvcAuthenticator(baseApiUrl, baseApiUrl, callbackUrl, memoryStore, 
+                publicConsumer, requestTokenStore);
+
+            var publicApplicationSettings = new ApplicationSettings
+                {
+                    BaseApiUrl = baseApiUrl,
+                    Consumer = publicConsumer,
+                    Authenticator = publicAuthenticator
+                };
+
+            // Partner Application Settings
+            ////var partnerConsumer = new Consumer(consumerKey, consumerSecret);
+
+            ////var partnerAuthenticator = new PartnerMvcAuthenticator(baseApiUrl, baseApiUrl, callbackUrl,
+            ////        memoryStore, signingCertificatePath, 
+            ////        partnerConsumer, requestTokenStore, signingCertificatePassword);
+
+            ////var partnerApplicationSettings = new ApplicationSettings
+            ////{
+            ////    BaseApiUrl = baseApiUrl,
+            ////    Consumer = partnerConsumer,
+            ////    Authenticator = partnerAuthenticator
+            ////};
+
+            // Pick one
+            // Choose what sort of application is appropriate. Comment out the above code (Partner Application Settings/Public Application Settings) that are not used.
+
+            _applicationSettings = publicApplicationSettings;
+            ////_applicationSettings = partnerApplicationSettings;
+        }
+
+        public static ApiUser User()
+        {
+            return new ApiUser { Name = Environment.MachineName };    
+        }
+
+        public static IConsumer Consumer()
+        {
+            return _applicationSettings.Consumer;
+        }
+
+        public static IMvcAuthenticator MvcAuthenticator()
+        {
+            return (IMvcAuthenticator)_applicationSettings.Authenticator;
+        }
+
+        public static IXeroCoreApi CoreApi()
+        {
+            if (_applicationSettings.Authenticator is IAuthenticator)
+            {
+                return new XeroCoreApi(_applicationSettings.BaseApiUrl, _applicationSettings.Authenticator as IAuthenticator,
+                    _applicationSettings.Consumer, User(), new DefaultMapper(), new DefaultMapper());
+            }
+           
+            return null;
+        }
+    }
+}
